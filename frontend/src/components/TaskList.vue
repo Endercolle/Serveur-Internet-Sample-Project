93b792e8cf90d05ecda2d36bf7ed531f54f3881d
Endercrolle <endercrolle@gmail.com>
2026-01-15 11:30:19 -0500

    repaire2

---
+++ frontend/src/components/TaskList.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="bg-white shadow p-4 rounded">
+    <li v-for="t in tasks" :key="t.id" class="border-b py-2">
+      {{ t.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["tasks"],
+};
+</script>
